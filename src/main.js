e064a121ddd82ba547681e4118f0140d6a6c4cb1
Ilona <ilonachorniy26@gmail.com>
2025-06-06 12:12:06 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './js/header.js';
+
+import './js/hero.js';
+
+import './js/artists.js';
+
+import './js/about.js';
+
+import './js/feedback.js';
+
+import './js/modal';
+
+import './js/mobile-menu.js';
+
+import './js/footer.js';
