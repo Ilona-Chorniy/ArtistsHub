9b7ee2f1b4a6081ae4a66aa777f143917fdbb204
Dmytro <dm.radchenko.f@gmail.com>
2025-06-07 21:24:32 +0300

    in process

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,28 +1,35 @@
-// import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
-// import styles bundle
 import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
-
 //===================================================================//
+import axios from 'axios';
+
+export async function GetFeedbacksResponse(query, page) {
+  const baseURL = 'https://sound-wave.b.goit.study/api/';
+  const endPoint = '/feedbacks';
+  const url = baseURL + endPoint;
+
+  const params = {
+    q: query,
+    page: page,
+    limit: 3,
+  };
+  const headers = {};
+
+  const res = await axios.get(url, { params, headers });
+
+  return res.data;
+}
