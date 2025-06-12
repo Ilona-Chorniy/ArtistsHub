d19b26091fb12bab4ddeb652abe2d12043e2a85f
kapillar <kapillarua@gmail.com>
2025-06-12 18:03:57 +0300

    fix: change path for src=""

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,19 +10,27 @@ import { renderAlbums } from './artistModal/renderAlbums.js';
 import { renderGenres } from './artistModal/renderGenres.js';
 import loader from './artistModal/utils/loader.js';
 
+let loaderTimerId = null;
+// час затримки для лоадера (в мілісекундах)
+const LOADER_DELAY_MS = 300;
+
 // Function to open the modal and fetch artist data
 export async function openModal(artistId) {
   //  Show the loader
-  loader.showArtistLoader();
 
   try {
     // Show the modal
     domRefs.modal.classList.remove('modal--hidden');
     document.body.classList.add('no-scroll');
 
+    // Show the loader after a delay
+    loaderTimerId = setTimeout(() => {
+      loader.showArtistLoader();
+    }, LOADER_DELAY_MS);
+
     // Get artist data
     const artistData = await fetchArtistData(artistId);
-
+    loader.showArtistLoader();
     // Clear old content
     clearModalContent();
 
@@ -39,6 +47,10 @@ export async function openModal(artistId) {
     alert('Artist data not found');
     closeModal();
   } finally {
+    if (loaderTimerId) {
+      clearTimeout(loaderTimerId);
+      loaderTimerId = null;
+    }
     // Hide the loader
     loader.hideArtistLoader();
   }
